f90a07c8fd9f4ebc04cd7e639bfef277d98a9b91
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2023-06-25 21:18:46 +0330

    Add Issue Input

--- src/js/Main/List.js
+++ src/js/Main/List.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "../../css/List.css"
+import AddTaskModal from "./AddTaskModal";
 
 const List = () => {
     const [taskList, setTaskList] = useState([]);
@@ -12,11 +13,13 @@ const List = () => {
 
     const fetchData = async () => {
         try {
-            const token = localStorage.getItem('Token')
-            const response = await axios.post('http://127.0.0.1:5000/allIssues?reporter=ma.banifatemi&jwt=' + token);
+            const token = localStorage.getItem('Token');
+            const username = localStorage.getItem('Username');
+            const response = await axios.post('http://127.0.0.1:5000/allIssues?reporter=' + username + '&jwt=' + token);
             setTaskList(response.data.message);
         } catch (error) {
             console.error('Error fetching data:', error);
+            // setTaskList((prevTasks) => prevTasks.filter((task) => task.id !== error.data.message.id));
         }
     };
 
@@ -32,13 +35,22 @@ const List = () => {
         }
     };
 
-    const deleteTask = async (taskId) => {
+    const deleteTask = async (taskId, taskReporter) => {
         try {
-            await axios.delete(`your-api-endpoint/${taskId}`);
+            const issueDataForDelete = {
+                id: taskId,
+                reporter: taskReporter
+            };
+            const token = localStorage.getItem('Token');
+            await axios.post(`http://127.0.0.1:5000/deleteIssue?jwt=` + token, issueDataForDelete);
             fetchData();
         } catch (error) {
             console.error('Error deleting task:', error);
         }
+    };
+
+    const handleAddTask = (taskData) => {
+        console.log(taskData);
     };
 
     return (
@@ -49,30 +61,34 @@ const List = () => {
                     type="text"
                     value={taskInput}
                     onChange={(e) => setTaskInput(e.target.value)}
-                    placeholder="Add a new task"
+                    placeholder="Search task ..."
                     className="form-control"
                 />
             </div>
-            <button onClick={addTask} className="btn btn-primary mb-3">Add Task</button>
+            <div className="mb-3">
+                <AddTaskModal onAddTask={handleAddTask} />
+            </div>
             <table className="table">
                 <thead className="table-dark">
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Summary</th>
+                    <th scope="col" className="text-center">ID</th>
+                    <th scope="col" className="text-center">Summary</th>
                     <th scope="col" className="text-center">Priority</th>
                     <th scope="col" className="text-center">Condition</th>
-                    <th scope="col">Actions</th>
+                    <th scope="col" className="text-center">Actions</th>
                 </tr>
                 </thead>
                 <tbody>
                 {taskList.map((task) => (
                     <tr key={task.id}>
-                        <td>{task.id}</td>
-                        <td>{task.summary.length > 50 ? `${task.summary.slice(0, 50)}...` : task.summary}</td>
+                        <td className="text-center">{task.id}</td>
+                        <td className="text-center">{task.summary.length > 50 ? `${task.summary.slice(0, 50)}...` : task.summary}</td>
                         <td className="text-center">{task.priority}</td>
                         <td className="text-center">{task.condition}</td>
-                        <td>
-                            <button onClick={() => deleteTask(task.id)} className="btn btn-danger">Delete</button>
+                        <td className="text-center">
+                            <button onClick={() => deleteTask(task.id, task.reporter)} className="btn btn-danger me-3">Delete</button>
+                            <button className="btn btn-info me-3">Show Info</button>
+                            <button className="btn btn-warning">Edit</button>
                         </td>
                     </tr>
                 ))}
