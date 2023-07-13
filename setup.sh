bfe88b5d8e904ff373d981ca2b7597dc7159a902
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2023-07-13 11:39:18 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,6 +16,6 @@ echo -e "${YELLOW}[local] docker rm -f -v TODO-List-Front-End${NC}"
 docker rm -f -v TODO-List-Front-End
 echo -e "${YELLOW}[local] docker run --name TODO-List-Front-End -d --network-alias todo-list-front-end -p 3000:3000 --network net_c34-122-0 todo-list-front-end:latest${NC}"
 docker run --name TODO-List-Front-End -d --network-alias todo-list-front-end -p 3000:3000 --network net_c34-122-0 todo-list-front-end:latest
-sleep 5
+sleep 10
 echo -e "${YELLOW}[local] curl --max-time 30 -s http://127.0.0.1:3000/buildDate.html${NC}"
 curl --max-time 30 -s http://127.0.0.1:3000/buildDate.html
