8081ae6af49aad55c762bd0611df9216c3d2dceb
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2023-07-23 19:25:35 +0330

    Add buildDate

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 set -e
+echo -e "${YELLOW}[local] buildDate=`date +"%Y-%m-%d %H:%M:%S "`+0330${NC}"
+buildDate=`date +"%Y-%m-%d %H:%M:%S "`+0330
+echo -e "${YELLOW}[local] echo ${buildDate} > public/buildDate.html${NC}"
+echo ${buildDate} > public/buildDate.html
 echo -e "${YELLOW}[local] cp src/sample-configs.js src/configs.js${NC}"
 cp src/sample-configs.js src/configs.js
 echo -e "${YELLOW}[local] sed -i "s+API_ADDRESS :.*+API_ADDRESS : \"API_ADDRESS\"+g" src/configs.js${NC}"
@@ -17,5 +21,18 @@ docker rm -f -v TODO-List-Front-End
 echo -e "${YELLOW}[local] docker run --name TODO-List-Front-End -d --network-alias todo-list-front-end -p 3000:3000 --network net_c34-122-0 todo-list-front-end:latest${NC}"
 docker run --name TODO-List-Front-End -d --network-alias todo-list-front-end -p 3000:3000 --network net_c34-122-0 todo-list-front-end:latest
 sleep 10
-echo -e "${YELLOW}[local] curl --max-time 30 -s http://127.0.0.1:3000/buildDate.html${NC}"
-curl --max-time 30 -s http://127.0.0.1:3000/buildDate.html
+i=1
+while [[ ${result} != ${buildDate} ]]; do
+    echo -e "${YELLOW}Try "$i" of 20 ...${NC}"
+    sleep 10
+    echo -e "${YELLOW}[local] curl --max-time 30 -s http://127.0.0.1:3000/buildDate.html${NC}"
+    result=`curl --max-time 30 -s http://127.0.0.1:3000/buildDate.html`
+    echo -e "${YELLOW}Expected Result: ${buildDate}${NC}"
+    echo -e "${YELLOW}Current  Result: ${result}${NC}"
+    ((i=i+1))
+    if [ $i == 20 ]; then
+        echo -e "${RED}Error in Deploy${NC}"
+        exit 1
+    fi
+done
+echo -e "${GREEN}Deployed Successfully${NC}"
